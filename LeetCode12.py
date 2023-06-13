d20707bbb9ee621f063a89f38b466f5a506a8f53
zhengzx <103814586+zzx0731@users.noreply.github.com>
2023-06-13 20:17:39 +0800

    LeetCode第12题 整数转罗马数字

---
+++ LeetCode12.py
@@ -0,0 +1,34 @@
+"""
+    整数转罗马数字
+"""
+
+VALUE_SYMBOLS = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+]
+
+
+def IntToRoman(num: int) -> str:
+    roman = list()
+    for value, symbol in VALUE_SYMBOLS:
+        while num >= value:
+            num -= value
+            roman.append(symbol)
+        if num == 0:
+            break
+    return "".join(roman)
+
+
+if __name__ == "__main__":
+    print(IntToRoman(2001))
